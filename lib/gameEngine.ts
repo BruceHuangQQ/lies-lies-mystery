b499a9f1e6734d7b1c80cc04358eeb689232cc10
Katsudon27 <152286098+Katsudon27@users.noreply.github.com>
2026-03-29 11:59:48 +1100

    refactor: move types to external file to be used by other files

--- lib/gameEngine.ts
+++ lib/gameEngine.ts
@@ -2,48 +2,12 @@ import gameData from "@/data/game-data.json";
 import fs from "fs";
 import path from "path";
 
-type Suspect = {
-    id: number;
-    name: string;
-};
-
-type Personality = {
-    id: number;
-    personality: string;
-};
-
-type Relationship = {
-    id: number;
-    relationship: string;
-    description: string;
-};
-
-type Weapon = {
-    id: number;
-    name: string;
-    description: string;
-};
-
-type Location = {
-    id: number;
-    name: string;
-};
-
-type Motive = {
-    id: number;
-    description: string;
-};
-
-type CaseData = {
-    suspects: Array<{
-        suspect: Suspect;
-        personality: Personality;
-        relationship: Relationship;
-    }>;
-    weapon: Weapon;
-    location: Location;
-    motive: Motive;
-};
+import {
+  CaseData,
+  Suspect,
+  Personality,
+  Relationship
+} from "@/lib/types/case";
 
 export class GameEngine {
     private data = gameData;
---
+++ lib/types/case.ts
@@ -0,0 +1,17 @@
+export type Suspect = { id: number; name: string };
+export type Personality = { id: number; personality: string };
+export type Relationship = { id: number; relationship: string; description: string };
+export type Weapon = { id: number; name: string; description: string };
+export type Location = { id: number; name: string };
+export type Motive = { id: number; description: string };
+
+export type CaseData = {
+  suspects: Array<{
+    suspect: Suspect;
+    personality: Personality;
+    relationship: Relationship;
+  }>;
+  weapon: Weapon;
+  location: Location;
+  motive: Motive;
+};
